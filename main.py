cf455b7d6212a074ec604b552c60293be4c80779
b.rowlingson <b.rowlingson@bde0bf32-9c99-11dd-8443-4fbe2d5f32ff>
2009-02-23 18:54:10 +0000

    added new topocolour plugin

---
+++ main.py
@@ -0,0 +1,52 @@
+#
+# Rasterlang (c) Barry Rowlingson 2008
+#
+#    This file is part of "rasterlang"
+#
+#    Rasterlang is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Rasterlang is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Rasterlang.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+from qgis.gui import *
+
+import resources_rc
+
+class MainPlugin(object):
+  def __init__(self, iface):
+    # Save a reference to the QGIS iface
+    self.iface = iface
+
+  def initGui(self):
+    # Create action
+    self.action = QAction(QIcon(":/icons/topocolour.png"),"TopoColour",self.iface.mainWindow())
+    self.action.setWhatsThis("Colour polygons by topology")
+    QObject.connect(self.action,SIGNAL("activated()"),self.run)
+    self.iface.addToolBarIcon(self.action)
+    self.iface.addPluginToMenu("&TopoColour",self.action)
+
+  def unload(self):
+    # Remove the plugin
+    self.iface.removePluginMenu("&TopoColour",self.action)
+    self.iface.removeToolBarIcon(self.action)
+
+  def run(self):
+    """ do the whole thing """
+    # choose a layer of polygons
+    # choose an attribute
+    # fire up the main dialog
+    pass
+
+    
+      
