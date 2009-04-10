0be9e86154a6314d319312f86a28eaec42d3580b
b.rowlingson <b.rowlingson@bde0bf32-9c99-11dd-8443-4fbe2d5f32ff>
2009-04-10 15:03:37 +0000

    handle non 1:N id values

--- topology.py
+++ topology.py
@@ -8,7 +8,7 @@ from qgis.gui import *
 
 from graph import Graph
 
-def compute(layer,fieldNum, idGraph  = False):
+def compute(layer, fieldNum, idGraph  = False):
     """ compute topology from a layer/field """
 
     f1 = QgsFeature()
@@ -25,8 +25,10 @@ def compute(layer,fieldNum, idGraph  = False):
     progress.setWindowModality(Qt.WindowModal)
     iop = 0
 
-    for l1 in range(1,layer.featureCount()):
-        layer.featureAtId(l1,f1)
+    ids = featureIds(layer)
+
+    for l1 in range(1,len(ids)):
+        layer.featureAtId(ids[l1],f1)
         a1 = f1.attributeMap()[fieldNum].toString()
         g1 = f1.geometry()
         for l2 in range(l1):
@@ -34,16 +36,25 @@ def compute(layer,fieldNum, idGraph  = False):
             if progress.wasCanceled():
                 return None
             iop=iop+1
-            layer.featureAtId(l2,f2)
+            layer.featureAtId(ids[l2],f2)
             g2 = f2.geometry()
             if g1.intersects(g2):
                 a2 = f2.attributeMap()[fieldNum].toString()
                 s.addEdge(a1,a2)
                 s.addEdge(a2,a1)
                 if idGraph:
-                    ig.addEdge(l1,l2)
+                    ig.addEdge(ids[l1],ids[l2])
     if not idGraph:
         return s
     else:
         return (s,ig)
 
+def featureIds(layer):
+    ids = []
+    p = layer.dataProvider()
+    allAttrs = p.attributeIndexes()
+    p.select(allAttrs)
+    f = QgsFeature()
+    while p.nextFeature(f):        
+        ids.append(f.id())
+    return ids
